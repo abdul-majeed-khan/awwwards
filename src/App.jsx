d83f6bc4be8900ec6f2080cbf4e8c15f92cf17a0
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-01-02 18:03:01 +0400

    app smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+import ScrollToPlugin from 'gsap/ScrollToPlugin'
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 
+// Register GSAP plugins
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+
 const App = () => {
+  useEffect(() => {
+    // Default ScrollTrigger configuration
+    ScrollTrigger.defaults({
+      start: "top center",
+      end: "bottom center",
+      scrub: 1,
+    });
+
+    // Configure smooth scrolling
+    gsap.to("html", {
+      scrollBehavior: "smooth",
+      scrollTrigger: {
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1
+      }
+    });
+
+    // Cleanup
+    return () => {
+      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+  }, []);
+
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
