e8bc6e8b3f7162b9beabbd2e8f2af6ff7b6575e6
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-01-04 16:21:53 +0400

    optimized-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,17 @@
-import { useEffect } from 'react'
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
-import ScrollToPlugin from 'gsap/ScrollToPlugin'
 import About from "./components/About"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Features from './components/Features'
-import MagneticParticles from './components/MagneticParticles'
+import MagneticParticles from "./components/MagneticParticles"
 
-
-// Register GSAP plugins
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 const App = () => {
-  useEffect(() => {
-    // Default ScrollTrigger configuration
-    ScrollTrigger.defaults({
-      start: "top center",
-      end: "bottom center",
-      scrub: 1,
-    });
-
-    // Configure smooth scrolling
-    gsap.to("html", {
-      scrollBehavior: "smooth",
-      scrollTrigger: {
-        start: "top top",
-        end: "bottom bottom",
-        scrub: 1
-      }
-    });
-
-    // Cleanup
-    return () => {
-      ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    };
-  }, []);
-
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <MagneticParticles />
       <Navbar />
       <Hero />
       <About />
+      <MagneticParticles />
       <Features />
     </main>
   )
