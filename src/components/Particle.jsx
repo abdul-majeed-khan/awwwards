8ca3d5d839dd546786b0f9dc1e3946d7aa9c1b6d
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-01-05 20:56:11 +0400

    particle section

---
+++ src/components/Particle.jsx
@@ -0,0 +1,14 @@
+import ParticleText from "./ParticleText";
+
+const Particle = () => {
+  return (
+    <section className="relative min-h-screen w-screen bg-white pb-52">
+        {/* Add the particle text as a background */}
+        <div className="absolute inset-0">
+            <ParticleText text="NEXUS" fontSize={120} />
+        </div>
+    </section>
+  )
+}
+
+export default Particle
