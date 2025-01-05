b35b36431cf70b6ed409bcae8e2192bfd73f2af0
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-01-05 20:55:30 +0400

    added text particle

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Features from './components/Features'
 import MagneticParticles from "./components/MagneticParticles"
+import Particle from "./components/Particle"
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Particle />
     </main>
   )
 }
