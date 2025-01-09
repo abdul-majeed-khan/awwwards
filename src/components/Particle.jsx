87626ba6e9025d5cfc610495583ffc5f163e4a2a
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2025-01-09 04:31:54 +0400

    particle update

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -4,11 +4,9 @@ import DigitalRain from "./DigitalRain"
 const Particle = () => {
   return (
     <section className="relative min-h-screen w-screen bg-white pb-52">
+      <DigitalRain />
         <div className="absolute inset-0">
             <ParticleText text="NEXUS" fontSize={120} />
-        </div>
-        <div className="absolute inset-0">
-            <DigitalRain />
         </div>
     </section>
   )
